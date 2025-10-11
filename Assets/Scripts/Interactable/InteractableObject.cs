45425cd2a62a27c6760edb21c186149f72ede511
roman <renkort33@gmail.com>
2025-10-11 17:52:47 +0300

    added interaction layer

--- Assets/Scripts/Interactable/InteractableObject.cs
+++ Assets/Scripts/Interactable/InteractableObject.cs
@@ -8,10 +8,13 @@ public class InteractableObject : MonoBehaviour, IInteractable
     [HideInInspector] public string InteractText;
     public Outline outline;
     public UnityEvent InteractAction;
+    [SerializeField] private LayerMask interactionLayer;
 
     void Awake()
     {
         SetOutline(false);
+        if (1 << gameObject.layer != interactionLayer)
+            Debug.LogWarning($"{gameObject} is not on interaction layer");
     }
 
     public void SetOutline(bool isActive)
